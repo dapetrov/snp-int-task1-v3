640af4302010806f9ad743205e017ff2082fc80a
sublystrum <petrov082011@mail.ru>
2023-02-19 20:57:17 +0300

    fix

--- menu-change-class.js
+++ menu-change-class.js
@@ -10,10 +10,10 @@ function modifyElement(elem) {
   const a = ["PONE", "PTWO", "PTHREE", "PFOUR", "PFIVE"];
   a.splice(a.indexOf(elem), 1);
   document.getElementById(elem).classList.add("p_hover__type-tour_one");
-  document.getElementById(elem).classList.remove("type-tour__two__p-hover");
+  document.getElementById(elem).classList.remove("type-tour__li");
   for (let i = 0; i < 4; i++) {
     const b = a[i];
-    document.getElementById(b).classList.add("type-tour__two__p-hover");
+    document.getElementById(b).classList.add("type-tour__li");
     document.getElementById(b).classList.remove("p_hover__type-tour_one");
   }
 }
