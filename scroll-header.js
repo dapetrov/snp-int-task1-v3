1d90981717cb406a1dcea2013a30305b7e03a7fc
sublystrum <petrov082011@mail.ru>
2023-03-01 20:31:57 +0300

    исправлена шапка при скролле

--- scroll-header.js
+++ scroll-header.js
@@ -2,9 +2,20 @@ const distance = 450;
 addEventListener("scroll", () => {
   let scrollDistance = window.scrollY;
   if (scrollDistance >= distance) {
-    document.getElementById("H__C__S").className =
-      "header-container_scroll__ON";
+    document.getElementById("header").className = "header_scroll";
+    const nav_li = document.getElementsByClassName("navigation__li");
+    Array.from(nav_li).forEach((element) => {
+      element.className = "navigation__li_del-border";
+    });
+    document.getElementById("header-logo").className = "header__logo_black";
   } else {
-    document.getElementById("H__C__S").className = "header-container_scroll";
+    document.getElementById("header").className = "header";
+    const nav_li_back = document.getElementsByClassName(
+      "navigation__li_del-border"
+    );
+    Array.from(nav_li_back).forEach((element) => {
+      element.className = "navigation__li";
+    });
+    document.getElementById("header-logo").className = "header__logo";
   }
 });
