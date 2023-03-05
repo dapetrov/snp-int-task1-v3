7f59a2bf296cf45ebad363b1a3f1bfc56d782ece
sublystrum <petrov082011@mail.ru>
2023-03-05 14:58:31 +0300

    бэм фикс

--- blocks/tour-type/tour-type.js
+++ blocks/tour-type/tour-type.js
@@ -9,11 +9,11 @@ a.map((element, index) => document.getElementById(element)).map(
 function modifyElement(elem) {
   const a = ["PONE", "PTWO", "PTHREE", "PFOUR", "PFIVE"];
   a.splice(a.indexOf(elem), 1);
-  document.getElementById(elem).classList.add("type-tour__li__button_active");
-  document.getElementById(elem).classList.remove("type-tour__li__button");
+  document.getElementById(elem).classList.add("tour-type__li__button_active");
+  document.getElementById(elem).classList.remove("tour-type__li__button");
   for (let i = 0; i < 4; i++) {
     const b = a[i];
-    document.getElementById(b).classList.add("type-tour__li__button");
-    document.getElementById(b).classList.remove("type-tour__li__button_active");
+    document.getElementById(b).classList.add("tour-type__li__button");
+    document.getElementById(b).classList.remove("tour-type__li__button_active");
   }
 }
